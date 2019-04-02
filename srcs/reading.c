af1c9a79620720fe5383e850c87569220fad9037
Filip <filip@fica.c>
2019-04-02 12:52:18 +0300

    minishell 02.04.19 fix bugs and fix non canon and fix signals and read

--- srcs/reading.c
+++ srcs/reading.c
@@ -6,7 +6,7 @@
 /*   By: filip <filip@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/30 21:53:57 by aashara-          #+#    #+#             */
-/*   Updated: 2019/04/02 12:32:30 by filip            ###   ########.fr       */
+/*   Updated: 2019/04/02 12:50:45 by filip            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,12 @@ void	set_input_mode(void)
 	struct termios	tty;
 
 	if (!isatty(0))
-		print_error("minishell", "stdin not terminal\n", NULL, 0); //EBADF, ENOTTY
-	tcgetattr(STDIN_FILENO, &savetty);//Save the terminal attributes so we can restore them later
+		print_error("minishell", "stdin not terminal\n", NULL, 0);
+	if (tcgetattr(STDIN_FILENO, &savetty) < 0)
+		print_error("minishell", "tcgetattr() error", NULL, 0);//EBADF, ENOTTY
+	//Save the terminal attributes so we can restore them later
 	 /*  Set the funny terminal modes. */
-    tcgetattr(STDIN_FILENO, &tty);// must check for err
+	tty = savetty;
 	 tty.c_lflag &= ~(ICANON| ECHO); /*  Clear ICANON and ECHO. */
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &tty);// must check
 }
@@ -38,7 +40,11 @@ char	*read_prompt(void)
 
 	set_input_mode();
 	i = -1;
-	buf = ft_strnew(LINE_MAX);
+	if (!(buf = ft_strnew(LINE_MAX)))
+	{
+		print_error("minishell", "malloc() error", NULL, ENOMEM);
+		return (NULL);
+	}
 	while (RUNNING)
 	{
 		read(STDIN_FILENO, &c, 1);
