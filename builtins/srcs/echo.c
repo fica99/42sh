390065a405f07d595177749feecd3f0fd4419d0a
Kugumi <pavialavia@gmail.com>
2019-11-28 18:24:13 +0300

    need to deal with \x \c and slashes

--- builtins/srcs/echo.c
+++ builtins/srcs/echo.c
@@ -12,17 +12,124 @@
 
 #include "ft_shell.h"
 
+int		ft_eflag(int i, char **argv, int argc, int *flags)
+{
+	int				j;
+
+	while (argv[i])
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (argv[i][j] == '\\' && argv[i][j + 1] == '\\' && argv[i][j + 2])
+			{
+				j += 2;
+				if (argv[i][j] == 'n')
+					ft_putchar_fd('\n', STDOUT_FILENO);
+				else if (argv[i][j] == 'a')
+					ft_putchar_fd('\a', STDOUT_FILENO);
+				else if (argv[i][j] == 'b')
+					ft_putchar_fd('\b', STDOUT_FILENO);
+				else if (argv[i][j] == 'f')
+					ft_putchar_fd('\f', STDOUT_FILENO);
+				else if (argv[i][j] == 'r')
+					ft_putchar_fd('\r', STDOUT_FILENO);
+				else if (argv[i][j] == 't')
+					ft_putchar_fd('\t', STDOUT_FILENO);
+				else if (argv[i][j] == 'v')
+					ft_putchar_fd('\v', STDOUT_FILENO);
+				else if (argv[i][j] == '\\')
+					ft_putchar_fd('\\', STDOUT_FILENO);
+				else if (argv[i][j] == '\\' && argv[i][j + 1] == 'c')
+				{
+					flags[0] = 1;
+					return (argc);
+				}
+				/*else if (argv[i][j] == '\x')
+					;*/
+				else
+				{
+					if (argv[i][j - 3] != '\\')
+						ft_putchar_fd('\\', STDOUT_FILENO);
+					ft_putchar_fd(argv[i][j], STDOUT_FILENO);
+				}
+			}
+			else if (argv[i][j] == '\\')
+				;
+			else
+				ft_putchar_fd(argv[i][j], STDOUT_FILENO);
+			j++;
+		}
+		if (argc != (i + 1))
+			ft_putchar_fd(' ', STDOUT_FILENO);
+		i++;
+	}
+	return (i);
+}
+
+int		ft_echoflags(int *flags, char **argv)
+{
+	int	i;
+	int				j;
+
+	i = 1;
+	ft_bzero(flags, 3 * sizeof(int));
+	while (argv[i] && argv[i][0] == '-' && argv[i][1])
+	{
+		j = 1;
+		while (argv[i][j])
+		{
+			if (argv[i][j] == 'n')
+				flags[0] = 1;
+			else if (argv[i][j] == 'e' || argv[i][j] == 'E')
+			{
+				if (argv[i][j] == 'e')
+					flags[1] = 1;
+				else
+					flags[1] = 0;
+			}
+			else
+				return (i);
+			j++;
+		}
+		i++;
+	}
+	return (i);
+}
+
 void	ft_echo(int argc, char **argv, char **env_cp)
 {
-	unsigned short	i;
+	int				i;
+	int				j;
+	int				flags[3];
 
+	i = ft_echoflags(flags, argv);
+	if (flags[1] == 1)
+		i = ft_eflag(i, argv, argc, flags);
 	(void)env_cp;
-	i = 0;
-	while (argv[++i])
+	while (argv[i])
 	{
-		ft_putstr_fd(argv[i], STDOUT_FILENO);
+		j = 0;
+		while (argv[i][j])
+		{
+			while (argv[i][j] == '\\')
+			{
+				if (argv[i][j + 1] == '\\')
+				{
+					ft_putchar_fd('\\', STDOUT_FILENO);
+					j += 2;
+				}
+				else
+					j++;
+			}
+			if (argv[i][j])
+				ft_putchar_fd(argv[i][j], STDOUT_FILENO);
+			j++;
+		}
+		i++;
 		if (argc != i)
 			ft_putchar_fd(' ', STDOUT_FILENO);
 	}
-	ft_putchar_fd('\n', STDOUT_FILENO);
+	if (flags[0] == 0)
+		ft_putchar_fd('\n', STDOUT_FILENO);
 }
