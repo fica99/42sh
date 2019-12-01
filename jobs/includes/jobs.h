9ce6bfb14e4ddc71bc03144361a557bdc1b73e81
mmarti <beachtelecom42@gmail.com>
2019-12-01 14:35:33 +0300

    add redir list to t_process

--- jobs/includes/jobs.h
+++ jobs/includes/jobs.h
@@ -10,6 +10,7 @@ typedef struct s_process
     char completed;             /* true if process has completed */
     char stopped;               /* true if process has stopped */
     int status;                 /* reported status value */
+    int **redir;
 }   t_process;
 
 /* A job is a pipeline of processes.  */
@@ -21,9 +22,6 @@ typedef struct s_job
     pid_t pgid;                 /* process group ID */
     char notified;              /* true if user told about stopped job */
     struct termios *tmodes;      /* saved terminal modes */
-    int fdin;
-    int fdout; /* standard i/o channels */
-    int fderr;
 }   t_job;
 
 /* The active jobs are linked into a list.  This is its head.   */
