b69fb1a13ca0f1d2cf2a8ccda2757efddaebe0ae
Kugumi <pavialavia@gmail.com>
2020-01-21 18:58:44 +0300

    type

---
+++ builtins/srcs/keywords.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keywords.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jijerde <jijerde@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/09 20:49:36 by jijerde           #+#    #+#             */
+/*   Updated: 2020/01/21 18:14:57 by jijerde          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_shell.h"
+
+char		*type_error(char *arg)
+{
+	ft_error("42sh", "type", "not found", arg);
+	return (NULL);
+}
+
+static void	push_keyw(t_keyw *keyw, char *name)
+{
+	while (keyw->next)
+		keyw = keyw->next;
+	if (!(keyw->next = ft_memalloc(sizeof(t_keyw))))
+		err_exit("42sh", "malloc() error", NULL, NOERROR);
+	keyw = keyw->next;
+	if (!(keyw->data = ft_strdup(name)))
+		err_exit("42sh", "malloc() error", NULL, NOERROR);
+	keyw->next = NULL;
+}
+
+void		fill_keyw(t_keyw *keyw)
+{
+	if (!(keyw->data = ft_strdup("!")))
+		err_exit("42sh", "malloc() error", NULL, NOERROR);
+	keyw->next = NULL;
+	push_keyw(keyw, "{");
+	push_keyw(keyw, "}");
+	push_keyw(keyw, "case");
+	push_keyw(keyw, "do");
+	push_keyw(keyw, "done");
+	push_keyw(keyw, "elif");
+	push_keyw(keyw, "else");
+	push_keyw(keyw, "esac");
+	push_keyw(keyw, "fi");
+	push_keyw(keyw, "for");
+	push_keyw(keyw, "if");
+	push_keyw(keyw, "in");
+	push_keyw(keyw, "then");
+	push_keyw(keyw, "until");
+	push_keyw(keyw, "while");
+}
+
+void		free_keyw(t_keyw *keyw)
+{
+	t_keyw *tmp;
+
+	free(keyw->data);
+	keyw = keyw->next;
+	while (keyw)
+	{
+		tmp = keyw->next;
+		free(keyw->data);
+		free(keyw);
+		keyw = tmp;
+	}
+}
--- term/srcs/global_var.c
+++ term/srcs/global_var.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   global_var.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aashara- <aashara-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jijerde <jijerde@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/21 20:57:34 by aashara-          #+#    #+#             */
-/*   Updated: 2020/01/08 19:48:10 by aashara-         ###   ########.fr       */
+/*   Updated: 2020/01/21 18:52:08 by jijerde          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	init_global_var(char **environ)
 	init_global_env(&g_set_env, values);
 	init_bin_table(&g_bin_table);
 	init_built_table(&g_built_table);
+	fill_keyw(&g_keyw);
 	init_curr_pwd();
 	set_env("?", "0", ALL_ENV);
 }
@@ -29,6 +30,7 @@ void	free_globar_var(void)
 {
 	free(g_curr_dir);
 	free_table(&g_bin_table);
+	free_keyw(&g_keyw);
 	unset_env_struct(&g_set_env);
 	unset_env_struct(&g_env);
 }
