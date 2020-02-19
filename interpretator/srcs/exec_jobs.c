3f0b1d226c8ae2ff743775a47bf1a52da5c3a04b
fica99 <fica.c@yandex.ru>
2020-02-20 02:44:57 +0300

    Fix norminette

--- interpretator/srcs/exec_jobs.c
+++ interpretator/srcs/exec_jobs.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_jobs.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ggrimes <ggrimes@student.42.fr>            +#+  +:+       +#+        */
+/*   By: aashara- <aashara-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/21 13:23:13 by mmarti            #+#    #+#             */
-/*   Updated: 2020/02/18 22:03:09 by ggrimes          ###   ########.fr       */
+/*   Updated: 2020/02/20 02:44:23 by aashara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,8 +72,8 @@ static int			log_check(t_job *j, t_job *last_job)
 		j_amount++;
 		j = j->next;
 	}
-	if (j && (separator == 0 || separator == T_AND || ((separator == T_AND_AND && bad_exit < j_amount)
-	|| (separator == T_OR_OR && bad_exit == j_amount))))
+	if (j && (separator == 0 || separator == T_AND || ((separator == T_AND_AND
+	&& bad_exit < j_amount) || (separator == T_OR_OR && bad_exit == j_amount))))
 		return (0);
 	return (-1);
 }
